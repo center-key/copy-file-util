34d39b8bd6b295fc73455788f0217cf64942a44e
dpilafian <dem@centerkey.com>
2022-11-07 18:01:28 -0800

    Release v0.1.5

--- dist/copy-file.umd.cjs
+++ dist/copy-file.umd.cjs
@@ -1,4 +1,4 @@
-//! copy-file-util v0.1.4 ~~ https://github.com/center-key/copy-file-util ~~ MIT License
+//! copy-file-util v0.1.5 ~~ https://github.com/center-key/copy-file-util ~~ MIT License
 
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
