7fa9776b05ae7626437e4b8af6ee87ab1f0ff829
dpilafian <dem@centerkey.com>
2024-01-02 15:04:49 -0800

    Release v1.1.3

--- dist/copy-file.d.ts
+++ dist/copy-file.d.ts
@@ -1,4 +1,4 @@
-//! copy-file-util v1.1.2 ~~ https://github.com/center-key/copy-file-util ~~ MIT License
+//! copy-file-util v1.1.3 ~~ https://github.com/center-key/copy-file-util ~~ MIT License
 
 export type Settings = {
     cd: string;
